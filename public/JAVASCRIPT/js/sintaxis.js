25928ea4efd4ab11a539903b8ec12f52580d4d61
elena-llanos <elena.llb@hotmail.com>
2023-07-10 10:54:20 +0200

    inicio javascript

---
+++ public/JAVASCRIPT/js/sintaxis.js
@@ -0,0 +1,37 @@
+ 'use strict';
+ 
+ 
+ // alert('hola');
+//   console.log('hola a todo el mundo');
+
+//   const nombre = prompt('Dime tu nombre');
+
+//   console.log('Hola ' + nombre);
+
+//   const number = prompt('dime un número');
+
+//   console.log(typeof number);// number es un string, 
+//   console.log(number + 2);//Aquí estamos concatenando, no sumando
+
+
+  const  numero = +prompt('dime un numero');//hay que convertirlo con un + por delante
+//   console.log(typeof numero);
+//   console.log(numero + 2); 
+
+
+
+  if(numero === 5){ //El triple igual compara también los tipo, no solo el valor
+    console.log("ok");
+  }else{
+    console.log('no es 5');
+  }
+
+
+  var variable = null;
+  var variable2 = +'jhjhkjh';
+  console.log(typeof variable); //null es de tipo object
+  console.log( variable); //null es de tipo object
+  console.log( !variable2); //
+
+
+  
