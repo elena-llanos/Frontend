cc7fa4a963138a595a1dca18341cbe957e5fe124
elena-llanos <elena.llb@hotmail.com>
2023-07-12 09:56:05 +0200

    crud básico

--- public/JAVASCRIPT/js/dom.js
+++ public/JAVASCRIPT/js/dom.js
@@ -1,15 +1,15 @@
-window.addEventListener('DOMContentLoaded' , function(){
-    let  h1 = this.document.querySelector('h1');
+window.addEventListener('DOMContentLoaded', function () {
+    let h1 = this.document.querySelector('h1');
 
     console.log(h1);
     h1.innerHTML = 'Modificado por JavaScript';
 
 
-    const input  = this.document.querySelector('input');
-    const boton  = this.document.querySelector('button');
-    const span  = this.document.querySelector('span');
+    const input = this.document.querySelector('input');
+    const boton = this.document.querySelector('button');
+    const span = this.document.querySelector('span');
 
-    boton.addEventListener('click' , function(){
+    boton.addEventListener('click', function () {
         span.innerHTML = 'Hola ' + input.value;
     });
 
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded' , function(){
 
     const ul = this.document.querySelector('ul');
 
-    for(let dato of coleccion){
+    for (let dato of coleccion) {
         const li = this.document.createElement('li');
         li.innerHTML = dato;
 
@@ -26,3 +26,6 @@ window.addEventListener('DOMContentLoaded' , function(){
 
     }
 });
+
+
+
